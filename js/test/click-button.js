17cb4c779dea855252590a3592b8cd07d7d41387
Roman <65686226+morozit@users.noreply.github.com>
2020-11-18 15:41:30 +0200

    Create click-button.js

---
+++ js/test/click-button.js
@@ -0,0 +1,141 @@
+import galleryItems from './gallery-items.js';
+
+
+const galleryRefsList = document.querySelector('.js-gallery');
+// TODO: створення розмітки
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const modal = document.querySelector('div.lightbox');
+const modalImg = document.querySelector('.lightbox__image');
+
+function createGalleryMarkup(img) {
+	return img
+		.map(({
+			preview,
+			original,
+			description
+		}) => {
+			return `<li class="gallery__item">
+                <a
+                  class="gallery__link"
+                  href="${original}"
+                >
+                  <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                  />
+                </a>
+              </li>`;
+		}).join("");
+}
+
+galleryRefsList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryRefsList.addEventListener('click', openModal);
+
+// TODO: Додатква ф-еція для зміни шляху та "альт"
+function changeModalImgAttributes(event) {
+	modalImg.src = event.target.dataset.source;
+	modalImg.alt = event.target.alt;
+}
+
+// TODO: Відкриття модалки, , добавлення слухачів
+function openModal(event) {
+	event.preventDefault();
+	if (event.target.nodeName !== 'IMG') {
+		return;
+	}
+	changeModalImgAttributes(event);
+	modal.classList.add('is-open');
+	window.addEventListener('keydown', keyboardPress);
+  modal.addEventListener('click', closeModal);
+  pressArrows.addEventListener('click', mousePress);
+}
+
+
+// ! не працює якщо передати ф-цію (закрниває модалку по кліку в любій області)
+// function eventTarget(event) {
+//   if (event.target.tagName === "IMG") {
+//     return;
+//   }
+//   else if (event.target.id === 'lightbox__container-img--btn') return;
+// }
+
+function closeModal(event) {
+  // ! не працює якщо передати ф-цію (закрниває модалку по кліку в любій області)
+  // eventTarget(event);
+
+  if (event.target.tagName === "IMG") {
+    return;
+  } 
+  // todo: коли дописую нижній код для реалізації пролистування по кліку, перестає правильно працювати. після кліку по button не працює ESC
+  else if (event.target.classList.contains ("lightbox__container-img--btn")) {
+    return;
+  } else if (event.target.dataset.action ("lightbox-button-press")) {
+    return;
+  }
+
+
+
+	modal.classList.remove('is-open');
+	modalImg.src = '';
+	window.removeEventListener('keydown', keyboardPress);
+  modal.removeEventListener('click', closeModal);
+  pressArrows.removeEventListener('click', mousePress);
+}
+
+
+const UrlsArr = galleryItems.map((img) => img.original);
+const UrlsArrLength = UrlsArr.length - 1;
+
+function keyboardPress(event) {
+	// TODO:
+	if (event.code === "ArrowLeft") {
+		if (toggleSlide(0, UrlsArrLength)) return
+		modalImg.src = UrlsArr[UrlsArr.indexOf(modalImg.src) - 1];
+		return
+	}
+	if (event.code === "ArrowRight") {
+		if (toggleSlide(UrlsArrLength, 0)) return;
+		modalImg.src = UrlsArr[UrlsArr.indexOf(modalImg.src) + 1];
+		return
+	}
+	// TODO: ESC
+	if (event.code === "Escape") {
+		closeModal(event);
+	}
+}
+// function toggleSlide(par1, par2) {
+// 	if (modalImg.src === UrlsArr[par1]) {
+// 		modalImg.src = UrlsArr[par2];
+// 		return true;
+// 	}
+// }
+
+const pressArrows = document.querySelector('[data-action="lightbox-button-press"]');
+
+function mousePress(event) {
+  if (event.target.dataset === pressArrows) {
+    return;
+  }
+
+  if (event.target.getAttribute === pressArrows) {
+		if (toggleSlide(0, UrlsArrLength)) return
+		modalImg.src = UrlsArr[UrlsArr.indexOf(modalImg.src) - 1];
+		return
+  }
+  
+	if (event.target.classList === pressArrows) {
+		if (toggleSlide(UrlsArrLength, 0)) return;
+		modalImg.src = UrlsArr[UrlsArr.indexOf(modalImg.src) + 1];
+		return
+	}
+  
+}
+
+function toggleSlide(par1, par2) {
+	if (modalImg.src === UrlsArr[par1]) {
+		modalImg.src = UrlsArr[par2];
+		return true;
+	}
+}
